621168d4be0547ae3e2f97c0c8bfbb42476639b2
Cheng Yang <chengyang@xiaomi.com>
2021-08-23 20:41:00 +0800

    use env variable
    Signed-off-by: Cheng Yang <chengyang@xiaomi.com>

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 $KYTHE/tools/http_server \
-  --public_resources /home/cy/Documents/Sources/Github/kythe/bazel-out/k8-fastbuild/bin/kythe/web/ui/resources/public \
+  --public_resources $KYTHE_ROOT_DIRECTORY/resources \
   --listen :8080 \
   --serving_table $KYTHE_OUTPUT_DIRECTORY_SERVING
